2aafde0f5dd4464457300a11a44365d289919d05
Yo Yehudi <yo@intermine.org>
2019-05-28 11:36:23 +0100

    add init script for testmine.

---
+++ init-testmine.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+if [ -z $(which wget) ]; then
+    # use curl
+    GET='curl'
+else
+    GET='wget -O -'
+fi
+
+cd $HOME
+
+# Pull in the server code.
+git clone --single-branch --branch 'dev' --depth 1 https://github.com/intermine/intermine.git server
+
+export PSQL_USER=postgres
+
+# Set up properties
+PROPDIR=$HOME/.intermine
+TESTMODEL_PROPS=$PROPDIR/testmodel.properties
+SED_SCRIPT='s/PSQL_USER/postgres/'
+
+mkdir -p $PROPDIR
+
+echo "#--- creating $TESTMODEL_PROPS"
+cp server/config/testmodel.properties $TESTMODEL_PROPS
+sed -i -e $SED_SCRIPT $TESTMODEL_PROPS
+
+# Initialise solr
+echo '#---> Setting up solr search'
+./server/config/travis/init-solr.sh
+
+# We will need a fully operational web-application
+echo '#---> Building and releasing web application to test against'
+(cd server/testmine && ./setup.sh)
+sleep 60 # let webapp startup
+
+# Warm up the keyword search by requesting results, but ignoring the results
+$GET "$TESTMODEL_URL/service/search" > /dev/null
+# Start any list upgrades
+$GET "$TESTMODEL_URL/service/lists?token=test-user-token" > /dev/null

9e34deda691e638385c9798ee37aeff9b4b90665
uosl <regen@anche.no>
2019-10-01 09:44:09 +0100

    Use custom project.xml to hopefully fix DB no space

--- init-biotestmine.sh
+++ init-biotestmine.sh
@@ -3,6 +3,7 @@
 set -e
 
 SCRIPT_DIR=`pwd`/scripts
+CONFIG_DIR=`pwd`/config/biotestmine
 
 if [ -z $(which wget) ]; then
     # use curl
@@ -36,7 +37,8 @@ $SCRIPT_DIR/init-solr.sh
 echo '#---> Setting up perl dependencies'
 $SCRIPT_DIR/init-perl.sh
 
-sed -i -e '29,39d' biotestmine/data/project.xml
+# Copy CI-specific config
+cp $CONFIG_DIR/* biotestmine/
 
 # We will need a fully operational web-application
 echo '#---> Building and releasing web application to test against'
